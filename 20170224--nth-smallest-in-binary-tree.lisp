962608d4207966f81d85e809c98dc7af52b0c599
Pascal J. Bourguignon <pjb@informatimago.com>
2017-03-03 13:50:19 +0100

    Deleted trailing whitespaces.

--- 20170224--nth-smallest-in-binary-tree.lisp
+++ 20170224--nth-smallest-in-binary-tree.lisp
@@ -1,15 +1,15 @@
 ;; https://programmingpraxis.com/2017/02/24/nth-smallest-item-in-binary-search-tree/
-;; 
+;;
 ;; Nth Smallest Item In Binary Search Tree
-;; 
+;;
 ;; by programmingpraxis
-;; 
+;;
 ;; A binary search tree is a binary tree in which all nodes in the left
 ;; subtree are less than the current node and all nodes in the right
 ;; subtree are greater than the current node. Items arrive in random
 ;; order, are inserted into the binary search tree, and an in-order
 ;; traversal produces the items in ascending order.
-;; 
+;;
 ;; Your task is to write a program that finds the nth smallest item in a
 ;; binary search tree, without enumerating all of the items in the
 ;; tree. When you are finished, you are welcome to read or run a
