3ab07ccb19e9e49040fcd2994b0e6017ff2c45bf
Pascal J. Bourguignon <pjb@informatimago.com>
2018-07-31 14:03:31 +0200

    Deleted trailing whitespaces.

--- 20180515--sum-embedded-numbers.lisp
+++ 20180515--sum-embedded-numbers.lisp
@@ -1,29 +1,29 @@
 ;; https://programmingpraxis.com/2018/05/15/sum-embedded-numbers/
 ;;
 ;; Sum Embedded Numbers
-;; 
+;;
 ;; May 15, 2018
-;; 
+;;
 ;; It’s mid-May, so at most universities the semester has ended and it is
 ;; safe for me to solve some of the exercises that students sent to me
 ;; over the last few months. Here’s a fun little one:
-;; 
-;; 
-;; 
+;;
+;;
+;;
 ;; Given a string containing embedded numbers, find the sum of the
 ;; embedded numbers. For instance, given the string “11aa22bb33cc44”, the
 ;; desired sum is 11 + 22 + 33 + 44 = 110. You may not use regular
 ;; expressions.
-;; 
-;; 
-;; 
+;;
+;;
+;;
 ;; Although the problem statement doesn’t say so, you may assume that the
 ;; numbers of interest are non-negative integers. Thus, the purpose of
 ;; the exercise is for students to iterate through a string, identify the
 ;; digits in the string, and manipulate them numerically.
-;; 
-;; 
-;; 
+;;
+;;
+;;
 ;; Your task is to write a program that sums the numbers embedded in a
 ;; string. When you are finished, you are welcome to read or run a
 ;; suggested solution, or to post your own solution or discuss the
@@ -55,4 +55,3 @@
 (test/sum-embedded-cardinals)
 ;; --> :success
 ;; [/sourcecode]
-
