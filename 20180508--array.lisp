d44aa3bd8a86ac038824dcd6f22c2aadff402d9b
Pascal J. Bourguignon <pjb@informatimago.com>
2019-01-27 15:20:21 +0100

    Indentation.

--- 20180508--array.lisp
+++ 20180508--array.lisp
@@ -1,21 +1,21 @@
 ;; An Array Exercise
-;; 
+;;
 ;; May 8, 2018
-;; 
+;;
 ;; We worked on linked lists in the previous exercise, so today we will
 ;; work on arrays:
-;; 
-;; 
-;; 
+;;
+;;
+;;
 ;; Given an array of distinct integers, replace each element of the array
 ;; with its corresponding rank in the array. For instance, the input
 ;; array [10,8,15,12,6,20,1] is replaced by the output array
 ;; [4,3,6,5,2,7,1] because element 1 has rank 1, element 6 has rank 2,
 ;; element 8 has rank 3, element 10 has rank 4, element 12 has rank 5,
 ;; element 15 has rank 6, and element 20 has rank 7.
-;; 
-;; 
-;; 
+;;
+;;
+;;
 ;; Your task is to write a program to replace array elements by their
 ;; rank. When you are finished, you are welcome to read or run a
 ;; suggested solution, or to post your own solution or discuss the
